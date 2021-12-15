99bb0f0608adb45f78788b9ac4fd8b9005fc86e9
Hiep Nguyen <ken87nguyen@gmail.com>
2021-12-15 21:02:18 +0700

    no-jira: adapt new method Application#GetWindow

--- src/FlaUI.Adapter.White/TestStack.White/Application.cs
+++ src/FlaUI.Adapter.White/TestStack.White/Application.cs
@@ -1,4 +1,8 @@
 ﻿using System.Diagnostics;
+using System.Threading;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
 
 namespace TestStack.White
 {
@@ -10,6 +14,12 @@ namespace TestStack.White
 
         public Application(Process process, bool isStoreApp = false) : base(process, isStoreApp)
         {
+        }
+
+        // TODO HYT 2021.12.15
+        public Window GetWindow(SearchCriteria searchCriteria, InitializeOption initializeOption)
+        {
+            throw new AbandonedMutexException();
         }
     }
 }
---
+++ src/FlaUI.Adapter.White/TestStack.White/Factory/InitializeOption.cs
@@ -0,0 +1,8 @@
+﻿namespace TestStack.White.Factory
+{
+    // TODO: HYT 2021.12.15
+    public enum InitializeOption
+    {
+        NoCache
+    }
+}
