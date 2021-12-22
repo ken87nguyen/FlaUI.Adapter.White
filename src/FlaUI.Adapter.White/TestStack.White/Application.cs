4816b231d3530f311a92cf9d4aaf41ae6b1743af
Hiep Nguyen <ken87nguyen@gmail.com>
2021-12-22 17:51:46 +0700

    no-jira: start application with FlaUI Adapter

--- src/FlaUI.Adapter.White/TestStack.White/Application.cs
+++ src/FlaUI.Adapter.White/TestStack.White/Application.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
-using TestStack.White.Factory;
-using TestStack.White.UIItems.Finders;
+using System.Linq;
+using FlaUI.Adapter.White.Resources;
+using FlaUI.Core;
+using TestStack.White.Configuration;
 using TestStack.White.UIItems.WindowItems;
 
 namespace TestStack.White
@@ -17,40 +19,77 @@ namespace TestStack.White
         {
         }
 
-        public Window GetWindow(SearchCriteria searchCriteria, InitializeOption initializeOption)
+        public Window GetWindow(AutomationBase automationBase)
         {
-            throw new Exception();
+            return new Window(base.GetMainWindow(automationBase).FrameworkAutomationElement);
         }
 
-        public Window GetWindow(string title)
+        public Window GetWindow(string title, AutomationBase automationBase)
         {
-            throw new Exception();
+            Window window = null;
+            Deprecated_WaitFor(() =>
+            {
+                var frameworkAutomationElement = base.GetAllTopLevelWindows(automationBase)
+                    .FirstOrDefault(x => x.Name == title)?.FrameworkAutomationElement;
+                window = frameworkAutomationElement != null ? new Window(frameworkAutomationElement) : null;
+                return window != null;
+            }, assertionMessage: String.Format(MessageResources.Application_NotFindWindow, title));
+            return window;
         }
 
-        public Process Process { get; set; } // TODO
+        public Process Process => Process.GetProcessById(base.ProcessId);
 
         public static Application Launch(ProcessStartInfo processStartInfo)
         {
-            // TODO HYT 2021.12.15
-            return null;
+            var app = FlaUI.Core.Application.Launch(processStartInfo);
+            return new Application(app.ProcessId);
         }
 
-        public static Application Launch(string processStartInfo)
+        public static Application Launch(string executable)
         {
-            // TODO HYT 2021.12.15
-            return null;
+            var app = FlaUI.Core.Application.Launch(executable);
+            return new Application(app.ProcessId);
         }
 
         public static Application Attach(Process process)
         {
-            // TODO HYT 2021.12.15
-            return null;
+            var app = FlaUI.Core.Application.Attach(process);
+            return new Application(app.ProcessId);
         }
 
-        public static Application Attach(string path)
+        public static Application Attach(string executable)
         {
-            // TODO HYT 2021.12.15
-            return null;
+            var app = FlaUI.Core.Application.Attach(executable);
+            return new Application(app.ProcessId);
+        }
+
+        private bool Deprecated_WaitFor(Func<bool> condition, Action action = null, string assertionMessage = null)
+        {
+            var now = DateTime.UtcNow;
+            while (true)
+            {
+                if (DateTime.UtcNow.Subtract(now).TotalSeconds > (new TimeSpan(0,0 , 0,0, CoreAppXmlConfiguration.Instance.FindWindowTimeout)).TotalSeconds)
+                {
+                    if (String.IsNullOrEmpty(assertionMessage))
+                    {
+                        return false;
+                    }
+                    throw new AutomationException(assertionMessage, nameof(Application));
+                }
+                try
+                {
+                    action?.Invoke();
+                    if (condition == null || condition())
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new AutomationException($@"An error occurred during WaitFor: {ex.Message}", ex.StackTrace);
+                }
+            }
+            return true;
         }
     }
 }
